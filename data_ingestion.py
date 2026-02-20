88977193f16d788b7fe66701aa904ab4e0ab0519
mkaniew2 <01178533@pw.edu.pl>
2026-02-20 11:19:45 +0100

    feat: loading and splitting pdf

---
+++ data_ingestion.py
@@ -0,0 +1,20 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+def load_and_chunk_pdf(
+    file_path: str, chunk_size: int = 400, chunk_overlap: int = 50
+) -> list[Document]:
+    """Loads a PDF file and splits it into smaller pieces"""
+    loader: PyPDFLoader = PyPDFLoader(file_path)
+    documents: list[Document] = loader.load()
+
+    splitter: RecursiveCharacterTextSplitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        separators=["\n\n", "\n", ".", " ", ""],
+    )
+
+    chunks: list[Document] = splitter.split_documents(documents)
+    return chunks
