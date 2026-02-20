51e68a93c168556e1d786a320cfef68a1a9e7ce7
mkaniew2 <01178533@pw.edu.pl>
2026-02-20 15:48:24 +0100

    fix: resolve text spacing issues using PyMuPDF

--- data_ingestion.py
+++ data_ingestion.py
@@ -1,4 +1,4 @@
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
@@ -7,7 +7,7 @@ def load_and_chunk_pdf(
     file_path: str, chunk_size: int = 400, chunk_overlap: int = 50
 ) -> list[Document]:
     """Loads a PDF file and splits it into smaller pieces"""
-    loader: PyPDFLoader = PyPDFLoader(file_path)
+    loader: PyMuPDFLoader = PyMuPDFLoader(file_path)
     documents: list[Document] = loader.load()
 
     splitter: RecursiveCharacterTextSplitter = RecursiveCharacterTextSplitter(
